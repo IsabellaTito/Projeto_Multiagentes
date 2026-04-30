4dab9a31949b15556d4ca8d927eb09e121ffc530
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2026-04-30 20:58:48 -0300

    feat: configurações para openrouter

--- src/agents/config/settings.py
+++ src/agents/config/settings.py
@@ -4,7 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY") 
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+LLM_PROVIDER = str(os.getenv("LLM_PROVIDER"))
 
 if not GOOGLE_API_KEY:
     raise ValueError ("GOOGLE_API_KEY não encontrada no .env")
+
+if not OPENROUTER_API_KEY:
+    raise ValueError ("OPENROUTER_API_KEY não encontrada no .env")
