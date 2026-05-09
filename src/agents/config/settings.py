ef680cc66556ff87e71ba0b85fe89d458d998add
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2026-05-09 10:12:11 -0300

    feat: atualiza configuracoes

--- src/agents/config/settings.py
+++ src/agents/config/settings.py
@@ -8,6 +8,8 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 LLM_PROVIDER = str(os.getenv("LLM_PROVIDER"))
+GEMINI_MODEL = str(os.getenv("GEMINI_MODEL"))
+OPENROUTER_MODEL = str(os.getenv("OPENROUTER_MODEL"))
 
 if not GOOGLE_API_KEY:
     raise ValueError ("GOOGLE_API_KEY não encontrada no .env")
