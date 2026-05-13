1942c52094c3f67c5954b9fcd89856a606b71525
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2026-05-12 21:53:16 -0300

    feat: adiciona página para login

--- src/shared/utils/__init__.py
+++ src/shared/utils/__init__.py
@@ -1,7 +1,9 @@
 from .session_handle import init_session
 from .config_sidebar import init_sidebar
+from .hello_app import hello_component
 
 __all__=[
     "init_session",
     "init_sidebar",
+    "hello_component",
 ]
