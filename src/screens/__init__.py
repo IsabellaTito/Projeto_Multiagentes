01996a99224dd1522f11a9f1c09b56805b84ffc5
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2026-05-12 21:54:38 -0300

    refactor: refatoração para incluir página de login

--- src/screens/__init__.py
+++ src/screens/__init__.py
@@ -1,9 +1,11 @@
 from screens.chat import ChatPage
 from screens.uploads import UploadsPage
 from screens.spreadsheet import SheetPage
+from screens.login import LoginPage
 
 __all__ = [
     "ChatPage",
     "UploadsPage",
-    "SheetPage"
+    "SheetPage",
+    "LoginPage",
 ]
